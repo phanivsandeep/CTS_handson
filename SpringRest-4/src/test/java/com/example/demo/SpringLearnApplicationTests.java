4ae59d9fa7897fa93006e78524849a01a9d5bd65
Chodavarapu Phani Visweswara Sandeep <46485155+phanivsandeep@users.noreply.github.com>
2021-07-10 03:20:29 +0530

    Update SpringLearnApplicationTests.java

--- SpringRest-4/src/test/java/com/example/demo/SpringLearnApplicationTests.java
+++ SpringRest-4/src/test/java/com/example/demo/SpringLearnApplicationTests.java
@@ -5,7 +5,6 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import org.junit.jupiter.api.Test;
-//import static org.junit.Assert.assertNotNull;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
@@ -23,7 +22,6 @@ class SpringLearnApplicationTests {
 
 	@Test
 	public void contextLoads() {
-//		assertNotNull(countryController);
 	}
 
 	@Test 
